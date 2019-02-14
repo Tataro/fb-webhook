f1fb011c3005a23e2a11b52f9a607ba0fe70d166
Tat Mac <tat.kittitat@gmail.com>
2019-02-14 14:22:30 +0700

    fix code

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.post('/webhook', (req, res) => {
     res.status(200).send('EVENT_RECEIVED')
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404)
+    // res.sendStatus(404)
   }
+  res.status(200).send('EVENT_RECEIVED')
 })
 
 app.get('/ping', (req, res) => {
